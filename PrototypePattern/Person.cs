3904e66adf05f62d4c7d982ed7c4c286c87deab7
amerina <wzyandi@gmail.com>
2021-01-22 13:58:03 +0800

    add Prototype pattern

---
+++ PrototypePattern/Person.cs
@@ -0,0 +1,51 @@
+﻿using FilterPattern;
+using PrototypePattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    public class Person : IPrototype
+    {
+        public string Name { get; set; }
+        public GenderType Gender { get; set; }
+        public MaritalStatus MaritalStatus { get; set; }
+
+        public ExFriend ExFriend { get; set; } = new ExFriend();
+
+        /// <summary>
+        /// Creates a shallow copy of the current System.Object.
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        /// <summary>
+        /// Creates a deep copy of the current System.Object.
+        /// </summary>
+        public object DeepClone()
+        {
+            var person = (Person)this.MemberwiseClone();
+            person.ExFriend = new ExFriend();
+            person.ExFriend.Name = this.ExFriend.Name;
+            return person;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name:{0},Gender:{1},MaritalStatus:{2},ExFriend:{3}", Name, Gender.ToString(), MaritalStatus.ToString(), ExFriend.Name);
+        }
+    }
+
+    public class ExFriend
+    {
+        public string Name { get; set; }
+
+    }
+
+}
--- PrototypePattern/Program.cs
+++ PrototypePattern/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FilterPattern;
+using System;
+using System.Collections.Generic;
 
 namespace PrototypePattern
 {
@@ -7,6 +9,38 @@ namespace PrototypePattern
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var personMySelf = new Person();
+            personMySelf.Name = "Amerina";
+            personMySelf.Gender = GenderType.Male;
+            personMySelf.MaritalStatus = MaritalStatus.Married;
+            personMySelf.ExFriend.Name = "Scarlett Johansson";
+
+            var CloneMan = (Person)personMySelf.Clone();
+            Console.WriteLine("Clone Man: " + CloneMan.ToString());
+
+            //CloneMan is a shallow copy,if you edit ex-friend,personMySelf will also change.
+            CloneMan.ExFriend.Name = "All Ex-friend is going, only left my wife,That's sad.";
+         
+            Console.WriteLine("Clone Man: " + CloneMan.ToString());
+            Console.WriteLine("Person My Self: " + personMySelf.ToString());
+
+            Console.WriteLine();
+
+            //Deep Clone
+            personMySelf.ExFriend.Name = "Scarlett Johansson";
+           
+            var DeepCloneMan = (Person)personMySelf.DeepClone();
+            Console.WriteLine("Deep Clone Man: " + DeepCloneMan.ToString());
+
+            DeepCloneMan.ExFriend.Name = "All Ex-friend is going, only left my wife,That's sad.";
+            Console.WriteLine("Deep Clone Man: " + DeepCloneMan.ToString());
+
+            Console.WriteLine("Person My Self: " + personMySelf.ToString());
+
+
         }
     }
+
+
 }
