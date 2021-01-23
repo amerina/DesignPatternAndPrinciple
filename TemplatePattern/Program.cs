88b437f3ac49cd56c2212d6f0b6234fc95a4e63a
amerina <wzyandi@gmail.com>
2021-01-23 09:29:54 +0800

    add proj

---
+++ TemplatePattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemplatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
