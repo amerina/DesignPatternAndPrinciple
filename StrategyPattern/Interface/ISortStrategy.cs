3c6d4f3475e60d65ab1de4a44d15379c86c530a0
amerina <wzyandi@gmail.com>
2021-01-25 17:33:34 +0800

    add StrategtPattern

---
+++ StrategyPattern/Interface/ISortStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern.Interface
+{
+    public interface ISortStrategy
+    {
+        void Sort(List<string> list);
+    }
+}
---
+++ StrategyPattern/QuickSort.cs
@@ -0,0 +1,17 @@
+﻿using StrategyPattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern
+{
+    public class QuickSort : ISortStrategy
+    {
+        public void Sort(List<string> list)
+        {
+            list.Sort();
+            Console.WriteLine("QuickSorted list ");
+
+        }
+    }
+}
